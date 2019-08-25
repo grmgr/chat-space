18dab5a0774a08f9e6da8bc697a36a1e50fb6751
shiwori <grmrgzm05@gmail.com>
2019-08-25 21:34:33 +0900

    edit_message-feature

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,60 @@
+$(function() {
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-message">
+                    <p class="upper-message__username">
+                      ${message.user_name}
+                    </p>
+                    <p class="upper-message__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="sub-message">
+                    <div class="sub-message">
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+  return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      console.log(data);
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      $('.form__submit').prop('disabled', false);
+
+      function scrollBottom(){
+        var target = $('.message').last();
+        var position = target.offset().top + $('.messages').scrollTop();
+        $('.messages').animate({
+          scrollTop: position
+        }, 300, 'swing');
+      }
+    })
+    .fail(function(){
+      alert('メッセージを入力してください')
+      $('.form__submit').prop('disabled', false);
+    })
+
+  })
+})

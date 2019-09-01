45aa02e10d1d4bcf86272b5a0fcd5b2dd76fd957
shiwori <grmrgzm05@gmail.com>
2019-09-01 18:43:42 +0900

    fix_incremental-search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,6 @@
-$(document).on('turbolinks:load',function(){
+// $(document).on('turbolinks:load',function(){
   $(function(){
-    var memberlist = [];
+
     function buildHTML(users){
       var html = `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${users.name}</p>
@@ -14,6 +14,7 @@ $(document).on('turbolinks:load',function(){
                   </div>`
       return html;
     }
+
     function addUser(id,name){
       var memberlist = $('#chat-group-form__memberlist');
       var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
@@ -23,6 +24,7 @@ $(document).on('turbolinks:load',function(){
                   </div>` 
       memberlist.append(html)
     }
+    
     $(".chat-group-form__input").on("keyup", function(){
       var input = $("#user-search-field").val();
       $.ajax({
@@ -36,28 +38,28 @@ $(document).on('turbolinks:load',function(){
       if (users.length !== 0) {
         users.forEach(function(user){
         var html = buildHTML(user)
-          $(`#user-search-result`).append(html)
+          $("#user-search-result").append(html)
         })
       }
       else {
         var html = appendNot("一致するワードがありません");
-        $(`#user-search-result`).append(html);
+          $("#user-search-result").append(html);
       }
       })
       .fail(function() {
         alert('検索に失敗しました');
     })
     });
-
-    $(document).on("click",".user-search-add", function(users){
-        var id = $(this).data('user-id');
-        var name = $(this).data("user-name");
+    
+    $("#user-search-result").on("click", ".user-search-add", function(){
+      var id = $(this).data('user-id');
+      var name = $(this).data("user-name");
         addUser(id,name);
         $(this).parent().remove();
     })
 
-    $(document).on("click",".user-search-remove",function(users){
+    $("#chat-group-form__memberlist").on("click",".user-search-remove", function(){
       $(this).parent().remove();
   })
 })
-})
+// })
